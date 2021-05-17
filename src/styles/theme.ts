8f682054905790e3a0320da112a822ee951df0de
Enio L. Sombra <eniosombra@gmail.com>
2021-05-17 16:26:01 -0300

    alteração nas cores do tema

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,11 +3,12 @@ import { extendTheme } from '@chakra-ui/react';
 export const theme = extendTheme({
   colors: {
     app: {
-      bg: '#13111b',
-      box: '#201b2c',
+      bg: '#1E1E2B',
+      box: '#2F3141',
       label: '#c9d1d9',
       text: '#EEEEF2',
       placeholder: '#8b949d',
+      selectedBorder: '#04d361',
     },
   },
   styles: {
